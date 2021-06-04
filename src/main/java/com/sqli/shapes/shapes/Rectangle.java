3df3b4c3b4b77d20ffcf2e973e03cb35955442c9
Rharnati Ismail <ismailrharnati2095@gmail.com>
2021-06-04 11:35:06 +0100

    Update Rectangle.java

--- src/main/java/com/sqli/shapes/shapes/Rectangle.java
+++ src/main/java/com/sqli/shapes/shapes/Rectangle.java
@@ -5,5 +5,5 @@ public class Rectangle extends Shape {
     public int calculateArea(String dimensions) {
         int[] dimension = formatDimensionsToNumbers(dimensions);
         return dimension[0] * dimension[1];
-    }
+    
 }
